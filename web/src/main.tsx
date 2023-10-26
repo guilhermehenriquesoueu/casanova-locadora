c55e75aed266a68b501f485d0c538c1b6ae5192c
Guilherme Henrique <guilhermehenriquesoueu@gmail.com>
2023-10-26 15:34:17 -0300

    Adding Hash Router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import { MyContextProvider } from './context/ContextApi.tsx'
 import Search from './components/search/Search.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
